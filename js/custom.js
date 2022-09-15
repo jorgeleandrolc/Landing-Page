c53c65763c7b1e34579139fe47703e590c12c849
Jorge Leandro <111012573+jorgeleandrolc@users.noreply.github.com>
2022-09-15 17:02:06 +0000

    Add Commit Final. :)

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,21 @@
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
-
 $(function () {
 
 	"use strict";
-
-	/* Preloader
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
 	}, 1500);
 
-	/* JQuery Menu
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
 	$(document).ready(function () {
 		$('header nav').meanmenu();
 	});
 
-	/* Tooltip
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
 	$(document).ready(function () {
 		$('[data-toggle="tooltip"]').tooltip();
 	});
 
-	/* sticky
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
 	$(document).ready(function () {
 		$(".sticky-wrapper-header").sticky({ topSpacing: 0 });
@@ -47,8 +34,6 @@ $(function () {
 		});
 	});
 
-	/* NiceScroll
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
 	$(".brand-box").niceScroll({
 		cursorcolor: "#9b9b9c",
